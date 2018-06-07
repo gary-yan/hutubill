fed04234be5ce3d81c5b81a4aca26a3fb63f654b
gary-yan <383791970@qq.com>
2018-06-07 02:58:56 +0200

    CategoryDAO

--- src/dao/CategoryDAO.java
+++ src/dao/CategoryDAO.java
@@ -1,3 +1,4 @@
+
 package dao;
 
 import java.sql.Connection;
@@ -9,15 +10,15 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import entity.Config;
+import entity.Category;
 import util.DBUtil;
 
-public class ConfigDAO {
-	// 获取config表中有多少行信息 统计总数
+public class CategoryDAO {
+	// 获取Category表中有多少行信息 统计总数
 	public int getTotal() {
 		int total = 0;
 		try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
-			String sql = "select count(*) from config";
+			String sql = "select count(*) from category";
 			// COUNT(*) 函数返回在给定的选择中被选的行数
 			ResultSet rs = s.executeQuery(sql);
 			while (rs.next()) {
@@ -30,28 +31,26 @@ public class ConfigDAO {
 		return total;
 	}
 	//增加
-	public void add(Config config) {
-		String sql = "insert into config values(null,?,?)";
+	public void add(Category category) {
+		String sql = "insert into category values(null,?)";
 		try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
-			ps.setString(1, config.key);
-			ps.setString(2, config.value);
+			ps.setString(1, category.name);
 			ps.execute();
 			ResultSet rs = ps.getGeneratedKeys();
 			if (rs.next()) {
 				int id = rs.getInt(1);
-				config.id = id;
+				category.id = id;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	//更新
-	public void update(Config config) {
-		String sql = "update config set key_=?, value=? where id= ?";// 空格对sql语句是否有影响
+	public void update(Category category) {
+		String sql = "update category set name= ? where id= ?";// 空格对sql语句是否有影响
 		try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
-			ps.setString(1, config.key);
-			ps.setString(2, config.value);
-			ps.setInt(3, config.id);
+			ps.setString(1, category.name);
+			ps.setInt(2, category.id);
 
 			ps.execute();
 
@@ -64,79 +63,76 @@ public class ConfigDAO {
 	public void delete(int id) {
 
 		try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
-			String sql = "delete from config where id = " + id;
+			String sql = "delete from category where id = " + id;
 			s.execute(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	//获取
-	public Config get(int id) {
-		Config config = null;
+	public Category get(int id) {
+		Category category = null;
 		try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
-			String sql = "select * from config where id =" + id;
+			String sql = "select * from category where id =" + id;
 			ResultSet rs = s.executeQuery(sql);
 			if (rs.next()) {
-				config = new Config();
-				String key = rs.getString("key_");
-				String value = rs.getString("value");
-				config.key = key;
-				config.value = value;
-				config.id = id;
+				category = new Category();
+				String name = rs.getString(2);//?? 1代表id 2 代表name
+				category.name = name;
+				category.id = id;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return config;
+		return category;
 	}
 	//查询所有
-	public List<Config> list() {
+	public List<Category> list() {
 		return list(0, Short.MAX_VALUE);
 	}
 	//分页查询
-	public List<Config> list(int start, int count) {
-		List<Config> configs = new ArrayList<Config>();
-		String sql = "select * from config order by id desc limit ?,?";
+	public List<Category> list(int start, int count) {
+		List<Category> categorys = new ArrayList<Category>();
+		String sql = "select * from category order by id desc limit ?,?";
 		try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
 			ps.setInt(1, start);
 			ps.setInt(2, count);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				Config config = new Config();
+				Category category = new Category();
 				int id = rs.getInt(1);
-				String key = rs.getString("key_");
-				String value = rs.getString("value");
-				config.id = id;
-				config.key = key;
-				config.value = value;
-				configs.add(config);
+				String name = rs.getString(2);
+				category.id = id;
+			
+				category.name = name;
+				categorys.add(category);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return configs;
+		return categorys;
 	}
 
-	public Config getByKey(String key) {
-		  Config config = null;
-		  String sql = "select * from config where key_ = ?";
-		  try(Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);)
-		  {
-			  ps.setString(1, key);
-			  ResultSet rs = ps.executeQuery();
-			  
-			  if(rs.next()) {
-				  config = new Config();
-				  int id = rs.getInt("id");
-				  String value = rs.getString("value");
-				  config.key = key;
-				  config.value = value;
-				  config.id = id;
-			  }
-			  }catch (SQLException e) {
-				  e.printStackTrace();
-			  }
-			return config;
-}
+//	public Category getByKey(String key) {
+//		  Category category = null;
+//		  String sql = "select * from category where key_ = ?";
+//		  try(Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);)
+//		  {
+//			  ps.setString(1, key);
+//			  ResultSet rs = ps.executeQuery();
+//			  
+//			  if(rs.next()) {
+//				  category = new Category();
+//				  int id = rs.getInt("id");
+//				  String value = rs.getString("value");
+//				  category.key = key;
+//				  category.value = value;
+//				  category.id = id;
+//			  }
+//			  }catch (SQLException e) {
+//				  e.printStackTrace();
+//			  }
+//			return category;
+//}
 
 }
